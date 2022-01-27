53479247df6d0248e6a1d8ab7a7a2cab6b56963f
Mansi-24 <98507100+Mansi-24@users.noreply.github.com>
2022-01-27 12:03:19 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+# Add header
+def add(x,y):
+    pass
+# Subtract header    
+def subtract(x,y):
+    pass
+# Multiply header    
+def multiply(x,y):
+    pass
+# Divide Header    
+def divide(x,y):
+    pass
